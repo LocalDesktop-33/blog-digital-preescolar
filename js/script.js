43c4d7d5c5e3b24a0b7e5d2107a7ecb814f7aef6
LocalDesktop-33 <134861442+LocalDesktop-33@users.noreply.github.com>
2026-02-21 23:17:14 -0600

    update files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".section-header").forEach(header => {
-    header.addEventListener("click", () => {
-      header.parentElement.classList.toggle("open");
-    });
+
+
+  const navLinks = document.querySelectorAll(".main-nav a");
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
   });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
 });
